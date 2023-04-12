e9d53e684110cd27a04218fd9dc97b667d16add0
Lervy Myriam <[javamyriam@gmail.com](mailto:javamyriam@gmail.com)>
2023-04-12 23:34:08 +0200

    Creation de la ddb

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -9,22 +9,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProgramController extends AbstractController
 
 {
+    #[Route('/program/', name: 'program_index')]
+    public function index(): Response
+    {
+        return $this->render('program/index.html.twig', [
+            'website' => 'Wild Series',
+        ]);
+    }
+    #[Route('/program/{id}', name: 'program_show' ,requirements: ['id' =>'\d+'], methods : ['GET'])]
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => '$id',
+        ]);
+    }
+};
 
- #[Route('/program/', name: 'program_index')]
- public function index(): Response
- {
-     return $this->render('program/index.html.twig', [
-         'website' => 'Wild Series',
-     ]);
-
-     {
-
-         return new Response(
-
-             '<html><body>Wild Series Index</body></html>'
-
-         );
-
-     }
- }
- }
