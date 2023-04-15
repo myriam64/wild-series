cbd125af80f3e801cb450bf4a30f932eb7983ebc
Lervy Myriam <[javamyriam@gmail.com](mailto:javamyriam@gmail.com)>
2023-04-15 19:55:01 +0200

    Quetes

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -23,6 +23,10 @@ class Program
     #[ORM\Column(length: 150)]
     private ?string $poster = null;
 
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Category $Category = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -60,6 +64,18 @@ class Program
     public function setPoster(string $poster): self
     {
         $this->poster = $poster;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->Category;
+    }
+
+    public function setCategory(?Category $Category): self
+    {
+        $this->Category = $Category;
 
         return $this;
     }
